c0306df6f0a7aa7c69fba4909f28660591d6a98b
Koushik Annareddy Sreenath <askoushik4@gmail.com>
2023-04-05 17:55:09 -0400

    Functionality test

--- test/test-script.py
+++ test/test-script.py
@@ -1,11 +1,12 @@
 import socket
 import sys
+import re
 
 input_file = sys.argv[1]
 expected_file = sys.argv[2]
 
 input = open("resource/" + input_file, "rb").read()
-expected_output = open("resource/" + expected_file, "r").read()
+expected_output = open("resource/" + expected_file, "rb").read()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 12345))
@@ -14,11 +15,11 @@ s.sendall(input)
 data = s.recv(4096)
 s.close()
 actual_output = data.decode('utf-8')
-
+actual_output = re.sub(" time=[0-9a-zA-Z:.]+\/>", "/>", actual_output)
 if actual_output == expected_output:
-    print("Account creation Test passed")
+    print("Test passed")
 else:
-    print("Account creation Test failed")
-    print("Expected: "+ expected_output)
+    print("Test failed")
+    print("Expected: " + str(expected_output))
     print("Actual: " + actual_output)
 
