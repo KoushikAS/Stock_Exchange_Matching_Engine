2ce1345f0aa7b42547164d70626d0e70b8571c44
Koushik Annareddy Sreenath <askoushik4@gmail.com>
2023-04-06 00:06:53 -0400

    Socket while loop

--- test/test-script.py
+++ test/test-script.py
@@ -6,20 +6,24 @@ input_file = sys.argv[1]
 expected_file = sys.argv[2]
 
 input = open("resource/" + input_file, "rb").read()
-expected_output = open("resource/" + expected_file, "rb").read()
-
+expected_output = open("resource/" + expected_file, "r").read()
+expected_output = re.sub("\n", "", expected_output)
+expected_output = re.sub("\t", "", expected_output)
+expected_output = re.sub(" ", "", expected_output)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 12345))
-print(input)
 s.sendall(input)
 data = s.recv(4096)
 s.close()
 actual_output = data.decode('utf-8')
 actual_output = re.sub(" time=[0-9a-zA-Z:.]+\/>", "/>", actual_output)
-if actual_output == expected_output:
+actual_output = re.sub("\n", "", actual_output)
+actual_output = re.sub("\t", "", actual_output)
+actual_output = re.sub(" ", "", actual_output)
+if repr(actual_output.strip()) == repr(expected_output.strip()):
     print("Test passed")
 else:
-    print("Test failed")
-    print("Expected: " + str(expected_output))
-    print("Actual: " + actual_output)
+    print("Test failed" )
+    print("Expected:" + repr(expected_output))
+    print("Actual:" + repr(actual_output))
 
