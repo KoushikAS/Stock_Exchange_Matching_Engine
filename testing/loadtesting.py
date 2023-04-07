320b6f69c76b60d00f12387823a83c8f075a549f
Ryan Mecca <rm358@duke.edu>
2023-04-06 22:25:46 -0400

    added a third thread

--- testing/loadtesting.py
+++ testing/loadtesting.py
@@ -2,6 +2,8 @@ import sys
 import threading
 import socket, time
 
+amt = 1000
+
 def place_order(input_file):
     input = open( input_file, "rb").read()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,14 +15,18 @@ def place_order(input_file):
 
 def buy_shares():
     print("Buying Share")
-    for i in range(1000):
+    for i in range(amt):
         place_order("resource/buyscriptloadtesting-input.txt")
 
 
 def sell_shares():
     print("Sell Share")
-    for i in range(1000):
+    for i in range(amt):
         place_order("resource/sellscriptloadtesting-input.txt")
+
+def query():
+    for i in range(amt):
+        place_order("resource/queryscriptloadTesting-input.txt")
 
 
 if __name__ =="__main__":
@@ -39,13 +45,16 @@ if __name__ =="__main__":
     for i in range(0, 10):
         t1 = threading.Thread(target=buy_shares, args=())
         t2 = threading.Thread(target=sell_shares, args=())
+        t3 = threading.Thread(targer=query, args=())
 
         startTime = time.time()
         t1.start()
         t2.start()
+        t3.start()
 
         t1.join()
         t2.join()
+        t3.join()
 
         endTime = time.time()
 
